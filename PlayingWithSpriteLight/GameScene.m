f626d08f7c1e51c3a649c6ab7dd858b09e920032
Michael Vilabrera <m.vilabrera@gmail.com>
2015-08-26 11:03:33 -0400

    initial commit

---
+++ PlayingWithSpriteLight/GameScene.m
@@ -0,0 +1,27 @@
+//
+//  GameScene.m
+//  PlayingWithSpriteLight
+//
+//  Created by Michael Vilabrera on 8/26/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import "GameScene.h"
+
+@implementation GameScene
+
+-(void)didMoveToView:(SKView *)view {
+    /* Setup your scene here */
+    
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+-(void)update:(CFTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+    
+}
+
+@end
